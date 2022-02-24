4a24f6bbd06bc933276aa2bc06da7352bcb46ed7
Nishanth <rajkumarnishanth@gmail.com>
2022-02-24 22:45:36 +0530

    [Nish] Manager class to manage Inventory
    A class added to generate inventory list from json file.
    Manager class manages all activities with each inventory.

---
+++ InventoryManagement/InventoryManager.cs
@@ -0,0 +1,21 @@
+﻿namespace InventoryManagement;
+
+public class InventoryManager
+{
+    private List<Inventory> myInventory;
+
+    public void Manage()
+    {
+        myInventory = InventoryFactory.GetInventory();
+        Display();
+    }
+
+    public void Display()
+    {
+        foreach (var item in myInventory)
+        {
+            Console.WriteLine(item.GetJsonString());
+            Console.WriteLine($"Item Value: Rs. {item.GetValue()}\n\n");
+        }
+    }
+}
